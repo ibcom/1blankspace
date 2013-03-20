935945be992f175f809a9678f85105a9a755f896
Mark Byers <mark.byers@ibcom.com.au>
2013-03-20 17:30:54 +1100

    setup.file

--- 1blankspace-2.0.0.js
+++ 1blankspace-2.0.0.js
@@ -304,6 +304,10 @@ ns1blankspace.scripts =
 	{
 		nameSpace: '1blankspace.supportIssue',
 		source: '/jscripts/1blankspace.supportissue-2.0.0.js'
+	},
+	{
+		nameSpace: '1blankspace.setup.file',
+		source: '/jscripts/1blankspace.setip.file-2.0.0.js'
 	}
 ]
 
