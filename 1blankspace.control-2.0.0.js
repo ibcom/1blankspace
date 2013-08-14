607bc377113e814340906c7b2442a4f34a758992
Mark Byers <mark.byers@ibcom.com.au>
2013-08-14 14:50:15 +1000

    control

--- 1blankspace.control-2.0.0.js
+++ 1blankspace.control-2.0.0.js
@@ -63,7 +63,7 @@ ns1blankspace.xhtml.logonNotes =
 	'<span style="font-weight:bold">Thanks!</span></span><br /><br />' +
 	'1blankspace all-of-enterprise is an example app based on the mydigitalstructure platform.' +
 	'<br /><br /><a href="http://mydigitalstructure.com/1blankspace" target="_blank">1blankspace namespace</a>' +
-	'<br /><br /><a href="https://github/ibcom/1blankspace" target="_blank">github repository</a>' +
+	'<br /><br /><a href="https://github.com/ibcom/1blankspace" target="_blank">github repository</a>' +
 	'<br /><br /><a href="http://mydigitalstructure.com/examples" target="_blank">More code examples</a>' +
 	'<br /><br /><a href="http://mydigitalstructure.com/documentation" target="_blank">Documentation</a>' +
 	'<br /><br /><a href="http://mydigitalstructure.com/gettingstarted" target="_blank">Getting started</a>' +
