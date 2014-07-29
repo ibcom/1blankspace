0f85d19a4d01531bbd97ad6c6fe54f4c505cb5fb
Mark Byers <mark.byers@ibcom.com.au>
2014-07-29 10:49:44 +1000

    whenDone with timeout setting

--- 1blankspace.util.whenDone-2.0.0.js
+++ 1blankspace.util.whenDone-2.0.0.js
@@ -1,30 +1,125 @@
+/*!
+ * Copyright 2010, ibCom Pty Ltd ATF ibCom Unit Trust & contributors
+ * Licensed under the MIT license.
+ * http://1blankspace.com/license
+ * 01 FEB 2010
+ *
+ * Reference: http://www.mydigitalstructure.com/advancedSearch_direct
+ *
+ * Example usage:
+ 		ns1blankspace.util.whenDone.init({manifest:
+		{
+			execute: 	'nsXXX.populate',
+			store: 		'nsXXX.data',
+			search:
+			[
+				{
+					name: 'bankAccount',
+					method: 'FINANCIAL_BANK_ACCOUNT_SEARCH',
+					criteria:
+					{
+						"fields":
+						[
+							{
+								"name": "lastreconciledamount"
+							}
+						],
+						
+						"sorts":
+						[
+							{
+								"name": "lastreconcileddate",
+								"direction": "desc"
+							}
+						],
+						"options":
+						{	
+							"rows": "1"
+						}
+					}
+				}
+			]
+		});
+ */
+
+
 ns1blankspace.util.whenDone =
 {
+	data: 		{},
+
 	init:		function (oParam)
 				{
 					var oManifest = ns1blankspace.util.getParam(oParam, 'manifest').value;
+					var bIsLoggedOn  = ns1blankspace.util.getParam(oParam, 'isLoggedOn', {"default": false}).value;
+					var iTimeout  = ns1blankspace.util.getParam(oParam, 'timeout').value;
+					var iTimer  = ns1blankspace.util.getParam(oParam, 'timer').value;
 
-					$.each(oManifest.search, function(i, oSearch)
+					if (!bIsLoggedOn)
 					{
 						$.ajax(
 						{
-							type: 'POST',
-							url: ns1blankspace.util.endpointURI(oSearch.method),
-							data: JSON.stringify(oSearch.criteria),
+							type: 'GET',
+							url: ns1blankspace.util.endpointURI('CORE_GET_USER_DETAILS'),
 							dataType: 'json',
-							success: function(data)
+							cache: false,
+							global: false,
+							success: function(data) 
 							{
-								ns1blankspace.util.whenDone.response(oParam, oSearch, data)
+								oParam = ns1blankspace.util.setParam(oParam, 'isLoggedOn', (data.status=='OK'));
+								ns1blankspace.util.whenDone.init(oParam);
 							}
+						});	
+					}
+					else
+					{
+						if (iTimeout !== undefined)
+						{
+							if (iTimer != 0) {clearTimeout(iTimer)};
+							var oErrorParam = $.extend(true, {}, oParam);
+							oErrorParam =  ns1blankspace.util.setParam(oErrorParam, 'error', true);
+						    iTimer = setTimeout('ns1blankspace.util.whenDone.callError(' + JSON.stringify(oErrorParam) + ')', iTimeout/1000);
+						    oParam = ns1blankspace.util.setParam(oParam, 'timer', iTimer);
+						}
+
+						$.each(oManifest.search, function(i, oSearch)
+						{
+							if (oSearch.uri != undefined)
+							{
+								$.ajax(
+								{
+									type: 'POST',
+									url: oSearch.uri,
+									data: oSearch.criteria,
+									dataType: 'json',
+									success: function(data)
+									{
+										ns1blankspace.util.whenDone.response(oParam, oSearch, data)
+									}
+								});
+							}
+							else
+							{
+								$.ajax(
+								{
+									type: 'POST',
+									url: ns1blankspace.util.endpointURI(oSearch.method),
+									data: JSON.stringify(oSearch.criteria),
+									dataType: 'json',
+									success: function(data)
+									{
+										ns1blankspace.util.whenDone.response(oParam, oSearch, data)
+									}
+								});
+							}	
 						});
-					});
+					}	
 				},
 
 	response: 	function (oParam, oSearch, oResponse)
 				{
 					var oManifest = ns1blankspace.util.getParam(oParam, 'manifest').value;
 					var oStore = ns1blankspace.util.toFunction(oManifest.store);
-					oStore[oSearch.name] = oResponse.data.rows;
+					oStore[oSearch.name] = oResponse.data;
 					ns1blankspace.util.whenDone.checkIfDone(oParam);
 				},
 
@@ -32,6 +127,8 @@ ns1blankspace.util.whenDone =
 				function (oParam)
 				{
 					var oManifest = ns1blankspace.util.getParam(oParam, 'manifest').value;
+					var iTimer = ns1blankspace.util.getParam(oParam, 'timer').value;
+					var bError = ns1blankspace.util.getParam(oParam, 'error', {"default": false}).value;
 
 					var iStoreLength = 0;
 					var oStore = ns1blankspace.util.toFunction(oManifest.store);
@@ -44,9 +141,16 @@ ns1blankspace.util.whenDone =
     					}
 					}
 
-					if (iStoreLength == oManifest.search.length)
+					if (!bError & (iStoreLength == oManifest.search.length))
 					{
-						ns1blankspace.util.execute(oManifest.execute, oParam)
+						if (iTimer != 0) {clearTimeout(iTimer)};
+						ns1blankspace.util.execute(oManifest.success, oParam)
 					}
-				}						
+				},
+
+	callError: 	function (oParam)
+				{
+					var oManifest = ns1blankspace.util.getParam(oParam, 'manifest').value;
+					ns1blankspace.util.execute(oManifest.error, oParam)
+				}								
 }	
