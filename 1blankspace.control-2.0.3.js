1e592770f402c4910c3a726dc903061a98a078d9
Mark Byers <mark.byers@ibcom.com.au>
2014-12-19 10:01:31 +1100

    imap count

--- 1blankspace.control-2.0.3.js
+++ 1blankspace.control-2.0.3.js
@@ -111,7 +111,7 @@ ns1blankspace.scripts.concat(
 	},
 	{
 		nameSpace: '1blankspace.messaging.imap',
-		source: '/jscripts/1blankspace.messaging.imap-2.0.1.js'
+		source: '/jscripts/1blankspace.messaging.imap-2.0.2.js'
 	},
 	{
 		nameSpace: '1blankspace.document',
