effc0e51d52f2a24eee774bf1a8c2789c4ed73ab
Mark Byers <mark.byers@ibcom.com.au>
2016-01-20 14:55:49 +1100

    structure data

--- 1blankspace.setup.structure-2.0.4.js
+++ 1blankspace.setup.structure-2.0.4.js
@@ -1453,6 +1453,15 @@ ns1blankspace.setup.structure =
 
 										aHTML.push('<tr class="ns1blankspaceCaption">' +
 														'<td class="ns1blankspaceCaption">' +
+														'Caption' +
+														'</td></tr>' +
+														'<tr class="ns1blankspaceTextMulti">' +
+														'<td class="ns1blankspaceTextMulti">' +
+														'<textarea rows="3" cols="35" id="ns1blankspaceSetupStructureElementCaption" class="ns1blankspaceTextMultiSmall" style="height: 50px;"></textarea>' +
+														'</td></tr>');
+
+										aHTML.push('<tr class="ns1blankspaceCaption">' +
+														'<td class="ns1blankspaceCaption">' +
 														'Reference' +
 														'</td></tr>' +
 														'<tr class="ns1blankspaceTextMulti">' +
@@ -1607,6 +1616,7 @@ ns1blankspace.setup.structure =
 											sData += '&id=' + ns1blankspace.util.fs(sID);
 											sData += '&title=' + ns1blankspace.util.fs($('#ns1blankspaceSetupStructureElementTitle').val());
 											sData += '&reference=' + ns1blankspace.util.fs($('#ns1blankspaceSetupStructureElementReference').val());
+											sData += '&caption=' + ns1blankspace.util.fs($('#ns1blankspaceSetupStructureElementCaption').val());
 											sData += '&alias=' + ns1blankspace.util.fs($('#ns1blankspaceSetupStructureElementAlias').val());
 											sData += '&datatype=' + ns1blankspace.util.fs($('input[name="radioDataType"]:checked').val());
 											sData += '&category=' + ns1blankspace.util.fs($('input[name="radioCategory"]:checked').val());
@@ -1692,6 +1702,7 @@ ns1blankspace.setup.structure =
 											$('#ns1blankspaceSetupStructureElementReference').val(oObjectContext.reference);
 											$('#ns1blankspaceSetupStructureElementAlias').val(oObjectContext.alias);
 											$('#ns1blankspaceSetupStructureElementHint').val(oObjectContext.hint);
+											$('#ns1blankspaceSetupStructureElementCaption').val(oObjectContext.caption);
 
 											var sHTML = (oObjectContext.description).formatXHTML();
 											tinyMCE.get('ns1blankspaceSetupStructureElementDescription' + ns1blankspace.counter.editor).setContent(sHTML);
