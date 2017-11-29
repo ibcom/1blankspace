5e94570cb07e3364b23e4cd242fb002cda8e05a6
ibCom <support@ibcom.biz>
2017-11-29 11:44:10 +1100

    saml

--- 1blankspace.control-2.4.6l.js
+++ 1blankspace.control-2.4.6l.js
@@ -275,7 +275,7 @@ ns1blankspace.scripts.concat(
 	},
 	{
 		nameSpace: '1blankspace.setup.website',
-		source: '/jscripts/1blankspace.setup.website-2.1.0.js'
+		source: '/site/312/1blankspace.setup.website-2.1.1.js'
 	},
 	{
 		nameSpace: '1blankspace.setup.websiteForm',
