c0aa72ec42743fb532330d26370e4833cd584404
ibCom <support@ibcom.biz>
2021-03-04 12:37:10 +1100

    external user

--- 1blankspace.control-3.6.7.js
+++ 1blankspace.control-3.6.7.js
@@ -397,7 +397,7 @@ ns1blankspace.scripts =
 	},
 	{
 		nameSpace: '1blankspace.setup.user',
-		source: '/site/1903/1blankspace.setup.user-3.6.5.js'
+		source: '/site/1903/1blankspace.setup.user-3.6.7.js'
 	},
 	{
 		nameSpace: '1blankspace.setup.userRole',
